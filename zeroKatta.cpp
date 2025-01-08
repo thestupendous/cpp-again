25fb672bead2bc88c7dd73ef20a102e2d9ec2761
Your Name <you@example.com>
2025-01-08 22:25:49 +0530

    zero katta thoda hua

--- zeroKatta.cpp
+++ zeroKatta.cpp
@@ -6,19 +6,29 @@
  * Won
  * Coords 
  * Score 
+ * ZeroKatta
 */
 
 Enum options { khali, zero, katta, };
 Enum players {pehla,dusra,koiNahi};
 
+
+class ZeroKatta 
+{
+	public:
+		options zeroKatta;
+};
+
+
+
 class Won{
 	public:
 		player vijeta;
 		bool jeeta;
-		//Won():vijeta(koiNahi),jeeta(false){};
+		Won():jeeta(false){};
 };
 
-int getInputInt()
+constexpr int getInputInt()
 {
 	int num;
 	std::cin>>num;
@@ -26,9 +36,14 @@ int getInputInt()
 }
 
 //functions 
-printYeWalaGameEndingText(won)
+//printGameStartingText();  +bad
+printYeWalaGameStartingText();
+//printGameEndingText(won);  +bad
+printYeWalaGameEndingText(won);
+//wannaContinue();  +bad
 
-class Coords{
+class Coords
+{
 	public:
 		int x,y;
 		Coords(int a,int b):x(a),b(b) {}
@@ -41,27 +56,79 @@ class Coords{
 
 class Box
 {
-	 options Khane[3][3]; // 3x3 matrix of zero-katta
-	 bool checkPossible(int x,int y); //check if placing move is correct
-	 Won checkWon(); //return if someone won, and who won
+	 ZeroKatta khane[3][3]; // 3x3 matrix of zero-katta
+	 bool checkPossible(int x,int y,player khiladi) //check if placing move is correct
+	 {
+		 //checck board ke bheetar ya nahi 
+		 if(x<0 || x>2 || y<0 || y>2)
+			 return false;
+
+		 //check khali jagah
+		 if(khane[x][y].kiska == koiNahi && khane[x][y].zeroKatta==khali ) 
+			 return true;
+
+		 return false;
+	 }
+
+
+	 Won checkWon() //return if someone won, and who won
 					 //TODO :ye error codes ke sath yes/no dega
 					 //matlab ek struct/class dega
-	public:
-	 placeMove(players,int x,int y,Won& won)
 	 {
-		 if(!checkPossible(x,y)) {
+		 //upar se niche
+		 if (( khane[0][0].zeroKatta == khane[1][0].zeroKatta &&
+				 khane[1][0].zeroKatta khane[2][0].zeroKatta )
+			 || ( khane[0][1].zeroKatta == khane[1][1].zeroKatta &&
+				 khane[1][1].zeroKatta khane[2][1].zeroKatta )
+			 || ( khane[0][2].zeroKatta == khane[1][2].zeroKatta &&
+				 khane[1][2].zeroKatta == khane[2][2].zeroKatta )
+
+			//baye se daye
+			 || ( khane[1][0].zeroKatta == khane[1][1].zeroKatta &&
+				 khane[1][1].zeroKatta khane[1][2].zeroKatta )
+			 || ( khane[0][0].zeroKatta == khane[0][1].zeroKatta &&
+				 khane[0][1].zeroKatta khane[0][2].zeroKatta )
+			 || ( khane[2][0].zeroKatta == khane[2][1].zeroKatta &&
+				 khane[2][1].zeroKatta == khane[2][2].zeroKatta )
+
+			// tirchhe
+			 || ( khane[0][0].zeroKatta == khane[1][1].zeroKatta &&
+				 khane[1][1].zeroKatta == khane[2][2].zeroKatta )
+			 || ( khane[2][0].zeroKatta == khane[2][1].zeroKatta &&
+				 khane[2][1].zeroKatta == khane[2][2].zeroKatta )
+			 )
+
+
+
+
+
+
+
+
+                                                       
+
+  
+
+	public:
+	 bool isBoardFull();
+	 placeMove(players player,int x,int y,Won& won)
+	 {
+		 if(!checkPossible(x,y,player)) {
 			 //TODO: dubara input lo
 			 return;
 		 }
-		 
-		 Khane[x][y]
+		
+		 khane[x][y].kiska = player;
+		 khane[x][y].zeroKatta = (static_cast<int>(player)==0)?zero:katta;
 
-			 
+		 if(checkWon()){
+			 won.vijeta = player;
+			 jeeta = true;
 		 }
 	 }
-	 bool boardFull();
 };
 
+#if 0 // +bad
 class Score
 {
 	public:  
@@ -74,22 +141,36 @@ class Score
 		;
 	}
 
-}
+};
+#endif
 	
 	 
 
 int main()
 {
-	int score[2] = {0,0};
+	//int score[2] = {0,0};  +bad
 	int turn;
-	bool userFlip=false;
+	//bool userFlip=false;  +bad
 
+#if 0  //keval ek game
+
+	printGameStartingText();
+	// andrew player 1, jhandrew player2
+	
 	while (1)  //kai sare khel
 	{ 
 		turn = (userFlip)?1:0;
 		Coords coords;
 		Won won;
 		Box khel();
+		printYeWalaGameStartingText();
+		// is bar player1 ya 2 pehle khelega
+#endif
+		Coords coords;
+		Won won;
+		Box khel();
+		turn = 0 // +bad
+
 		while (1)  //khel khilane ke liye
 		{
 
@@ -99,21 +180,21 @@ int main()
 					//jiska turn ho wahi input de !
 
 			khel.placeMove(turn,coords,won);
-				// matlab checksahi 1.moove, 2.placeMove
+				// matlab 1.checksahi moove, 2.placeMove
 				// move ke bad bhi jeeta ya nai, yahi func 
 				// evaluate karega, -> 3.checkWon 
 
 			if(won.jeeta) {
 				break;
 			}
-			if (khel.boardFull()){
+			if (khel.isBoardFull()){
 				break;
 			}
 
 			turn = (turn==0) ? 1  : 0 ;
 		}
 
-
+#if 0  //keval ek game
 		// iske bajaye sab ek function me
 		printYeWalaGameEndingText(won)
 		updateScore(won); // yaha par
@@ -127,7 +208,7 @@ int main()
 
 		userFlip = !userFlip;
 	}
-
+#endif
 
 }
 
