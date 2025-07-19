b1ec00eb168b774e031eb30e3181aacedb166747
Anand Kumar <stupendousji@gmail.com>
2025-07-19 14:18:18 +0530

    updated bst, heap, not finished

---
+++ datastructures/bst.cpp
@@ -0,0 +1,123 @@
+
+#include<iostream>
+using namespace std;
+
+struct tnode {
+	int data;
+	tnode *left;
+	tnode *right;
+};
+
+class bst {
+	tnode *root;
+
+	void traverse2(tnode *r2) {
+		if (r2 == nullptr) return;
+
+		traverse2(r2->left);
+
+		std::cout << r2->data << ',';
+		
+		traverse2(r2->right);
+	}
+
+public:
+	bst():root(nullptr) {}
+
+	void insert(int value) {
+		tnode *newNode = new tnode{value,nullptr,nullptr};
+		if ( root == nullptr ) {
+			root = newNode;
+			return;
+		}
+		tnode *p = root;
+		while ( p->left != nullptr || p->right != nullptr ) {
+
+			if ( value < p->data ) {
+				// go left
+				if (p->left != nullptr) {
+					p = p->left;
+				} else {
+					p->left = newNode;
+					return;
+				}
+			} else {
+				// go right
+				if (p->right != nullptr) {
+					p = p->right;
+				} else {
+					p->right = newNode;
+					return;
+				}
+			}
+		}
+	  if ( value < p->data ) {
+	  	p->left = newNode;
+	  	return;
+	  } else {
+	  	p->right = newNode;
+	  	return;
+	  }
+	}
+
+	void insertWithLogging(int value) {
+		std::cout << "inserting " << value << '\n' ;
+		tnode *newNode = new tnode{value,nullptr,nullptr};
+		if ( root == nullptr ) {
+			root = newNode;
+			return;
+		}
+		tnode *p = root;
+		while ( p->left != nullptr || p->right != nullptr ) {
+		  std::cout << "  inwhile, at " << value << '\n' ;
+
+			if ( value < p->data ) {
+		    std::cout << "  value less than " << p->data << " going left\n" ;
+				// go left
+				if (p->left != nullptr) {
+					p = p->left;
+				} else {
+					p->left = newNode;
+					std::cout << "  inserted left\n";
+					return;
+				}
+			} else {
+		    std::cout << "  value more than " << p->data << " going right\n" ;
+				// go right
+				if (p->right != nullptr) {
+					p = p->right;
+				} else {
+					p->right = newNode;
+					std::cout << "inserted right\n";
+					return;
+				}
+			}
+		}
+	  if ( value < p->data ) {
+	  	p->left = newNode;
+	  	return;
+	  } else {
+	  	p->right = newNode;
+	  	return;
+	  }
+	}
+
+	void traverse() {
+		std::cout << "whole tree : ";
+		traverse2(root);
+		std::cout << '\n';
+	}
+};
+
+int main() {
+	std::cout << '\n';
+	int input [] = {5,3,10,6,12};
+	bst t1;
+	for ( int i : input ) {
+		t1.insert(i);
+	}
+
+	t1.traverse();
+	
+	return 0;
+}
--- datastructures/heap.cpp
+++ datastructures/heap.cpp
@@ -2,16 +2,56 @@
 #include<vector>
 using std::cout, std::vector;
 
+void swap(int &a,int &b) {
+	int t = a;
+	a= b;
+	b= t;
+}
+
 class Heap {
 	vector<int> heapVec;
+	unsigned int last;
 
 public:
 
-	Heap()  {}
-	Heap(vector<int> &vec): heapVec(vec.size()+1) {}
+	Heap(): last(0), heapVec(1)  {}
+	Heap(vector<int> &vec) {
+		if (heapVec.size() == 0) {
+			heapVec.push_back(0);
+			last = 0;
+		}
+		//heapVec.insert(heapVec.end(),vec.begin(),vec.end());
+		cout << "  before\n";
+		print();
+		for ( int value : vec ) {
+			cout << "pushing " << value << '\n';
+			push(value);
+		}
+	}
 
-	void push(){
+	void push(int value){
+		cout <<"inserting: " << value << '\n';
 
+		if (last==0) {
+			cout << "empty heap, pushing and exiting.\n";
+			last = 1;
+			heapVec[++last] = value;
+			return;
+		} else {
+			++last;
+			heapVec.push_back(value);
+		  print();
+			//compare parents
+			int i = last;
+			while (i/2 >= 1 ) {
+				if (heapVec[i] > heapVec[i/2]) {
+					swap(heapVec[i],heapVec[i/2]);
+					cout << "  swapping " << heapVec[i] << " and " << heapVec[i/2] << '\n';
+				}
+				i /= 2;
+
+			}
+		}
 	}
 
 	int pop(){
@@ -31,6 +71,9 @@ public:
 int main() {
 	vector<int> v1{5,2,6,10,1,11};
 	Heap h1(v1);
+	h1.print();
+	cout << "pushing 200\n";
+	h1.push(200);
 	h1.print();
 
 	return 0;
